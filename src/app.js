0068e3e312f561d6866b0bab20768874f966d8d7
CodeMacaronesia <93927775+CodeMacaronesia@users.noreply.github.com>
2022-01-17 20:56:39 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
+asdasdasdasdasdasdasdasd
 //environment variables
 //Explicación de process.env si en el ordenador que estoy corriendo justo ahora existe un puerto definido úsalo, si no existe usa el 4000
 app.set ('port', process.env.PORT || 4000);
